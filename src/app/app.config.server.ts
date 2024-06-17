462700d538e773ebda52213d942724993cc9ac42
Anh Pham <anh.is.a.dev@gmail.com>
2024-06-17 18:20:53 +0700

    Update missing service for SSR

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,12 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { AuthService } from '@auth0/auth0-angular';
+import { AuthService, AuthClientConfig } from '@auth0/auth0-angular';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    { provide: AuthService, useValue: {} }
+    { provide: AuthService, useValue: {} },
+    { provide: AuthClientConfig, useValue: {} }
   ]
 };
 
