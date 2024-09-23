0f4d9b9f2523374c63aefd74ed387c6b0043d77f
Anh Pham <anh.is.a.dev@gmail.com>
2024-09-23 14:22:46 +0700

    Update server config

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,14 +1,20 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
 import { AuthService, AuthClientConfig } from '@auth0/auth0-angular';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    provideHttpClient(
+      withFetch()
+    ),
+    provideRouter(routes),
     { provide: AuthService, useValue: {} },
     { provide: AuthClientConfig, useValue: {} }
   ]
 };
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const config = mergeApplicationConfig(serverConfig);
